ffbaf35966d705cccfa8af50b9e7b573fb9e45ee
Vlad Halimov <sotto36623@gmail.com>
2024-12-25 20:05:19 +0300

    init slider28

--- main.js
+++ main.js
@@ -14,31 +14,26 @@ class Slider {
 	initSlider({
 		btnNext = null,
 		btnPrev = null,
-		containerSlider,
 		slider, item,
 		itemLength }) {
 		this.flagDragDropMouse = false; // Флаг dragDrop для desktop
 		this.flagDragDropTouch = false; // Флаг dragDrop для touch устройств
-		this.flagSliderCount = false;  // Флаг для подсчета шагов слайдера
-		this.flagSliderNumberStep = false; //Флаг отображения общего количества шагов и текущего шага
-		
+
 
 		this.windowWidth = document.documentElement.clientWidth;  // Ширина окна
 
 		this.elemItem = item;  // Один элемент слайдера
 		this.elemBtnNext = btnNext;  // Кнопка для перехода к следующему слайду
 		this.elemBtnPrev = btnPrev;  // Кнопка для перехода к предыдущему слайду
-		// this.elemContainerSlider = containerSlider;  // Контейнер слайдера
 		this.elemSlider = slider;  // Сам слайдер
-		this.elemTotalStep = null // элемент для отображения общего кол-ва шагов 
-		this.elemStepSlide = null //// элемент для отображения текущего шага
+
 
 		this.stepNumber = 1; // Текущий шаг
 		this.position = 0;  // Начальная позиция слайдера
 		this.sliderLength = itemLength;  // Количество элементов item слайдера
 		this.visibleSlides = this.getVisibleSlidesMediaQueries(this.mediaQueries);  // Количество видимых слайдов
 		this.distance = this.updateWidthItem();  // Ширина одного элемента слайдера
-		this.totalSteps = null; // 
+		this.setTotalSteps();
 
 		this.onResize = this.handleResize.bind(this); // обработчик события resize
 		this.onDOMLoaded = this.handleDOMLoaded.bind(this); // обработчик события DOMContentLoaded
@@ -63,39 +58,24 @@ class Slider {
 		}
 	}
 
-
-
 	dispatchSlideChangeEvent() {
-    // Создаем новое пользовательское событие 'slideChanged'
-    const event = new CustomEvent('slideChanged', {
-        // Передаем дополнительные данные в свойстве detail
-        detail: {
-            currentStep: this.stepNumber, // Текущий шаг слайдера
-            totalSteps: this.totalSteps,   // Общее количество шагов в слайдере
-        },
-    });
+		const event = new CustomEvent('slideChanged', {
+			bubbles: true,
+			detail: {
+				currentStep: this.stepNumber,
+				totalSteps: this.totalSteps,
+			},
+		});
 
-    // Отправляем (диспатчим) событие на элемент слайдера
-    this.elemSlider.dispatchEvent(event); // Отправка события
-}
-
-
-
-	// Метод инициализации подсчета шагов
-	initCount($totalStep = null, $stepSlide = null) {
-
-		this.flagSliderCount = true;  //  включение флага подсчета шагов
-
-		if (!$totalStep || !$stepSlide) return;
-
-		this.elemTotalStep = $totalStep;  // Элемент для отображения общего количества шагов
-		this.elemStepSlide = $stepSlide;  // Элемент для отображения текущего шага
-		this.elemStepSlide.textContent = this.stepNumber;
-		this.flagSliderNumberStep = true;
+		this.elemSlider.dispatchEvent(event);
 	}
 
-	initStepCallback(callBack) {
-		this.callBack = callBack;
+	initStepsCallback(callBack) {
+		this.callBackSteps = callBack;
+	}
+	
+	initResizeCallback (callback) {
+		this.callBackResize = callback;
 	}
 
 
@@ -132,47 +112,37 @@ class Slider {
 	handleClickNext() {
 		this.moveNext();
 		this.updateButtonStates();
-		if (this.flagSliderCount) {
-			this.showSlideStep();
-			this.dispatchSlideChangeEvent(); 
-		}
+		this.setSlideStep();
 	}
 
 	handleClickPrev() {
 		this.movePrev();
 		this.updateButtonStates();
-
-		if (this.flagSliderCount) {
-			this.showSlideStep();
-			this.dispatchSlideChangeEvent(); 
-		}
+		this.setSlideStep();
 	}
 
 	handleResize() {
+
 		let newWindowWidth = document.documentElement.clientWidth;
 		if (newWindowWidth === this.windowWidth) return;
 		this.resetSlider();
 		this.distance = this.updateWidthItem();
 		this.visibleSlides = this.getVisibleSlidesMediaQueries(this.mediaQueries);
-
-		if (this.flagSliderCount) {
-			this.setCountTotalStep();  // Установка общего количества шагов
-			if (!this.flagSliderNumberStep) return;
-			this.showTotalStep();  // Отображение общего количества шагов
-		}
-		this.windowWidth = newWindowWidth;
 		this.updateButtonStates();
+		this.setTotalSteps();
+		this.windowWidth = newWindowWidth;
+		this.dispatchSlideChangeEvent();
+		this.callBackSteps(this.stepNumber, this.totalSteps);
+		this.callBackResize();
 	}
 
 	handleDOMLoaded() {
-		// this.elemSlider.style.transition = 'transform .3s linear';  // Установка анимации перехода
-		if (this.flagSliderCount) {
-			this.setCountTotalStep();  // Установка общего количества шагов
-			if (!this.flagSliderNumberStep) return;
-			this.showTotalStep();  // Отображение общего количества шагов
-		}
 		this.updateButtonStates();
+		this.setTotalSteps();
+		this.dispatchSlideChangeEvent();
+		this.callBackSteps(this.stepNumber, this.totalSteps);
 	}
+
 
 	initDragDrop(desktop = false) {
 		this.flagDragDropTouch = true;
@@ -213,7 +183,7 @@ class Slider {
 		const clientX = e.touches ? e.touches[0].clientX : e.clientX;
 
 		this.startDragDrop(clientX);
-		
+
 	}
 
 	handleMove(e) {
@@ -227,7 +197,7 @@ class Slider {
 		if (!this.isDragging) return;
 		// e.preventDefault();
 		this.endDragDrop();
-		
+
 	}
 
 	// Метод начала перетаскивания
@@ -253,35 +223,35 @@ class Slider {
 			if (this.position > 0) {
 				this.animateSlider(this.elemSlider, 0);
 				this.position = 0;
-				if (this.flagSliderCount) {
-					this.showSlideStep();
-				}
+
+				this.setSlideStep();
+
 				// Если позиция меньше конца слайдера, вернуться к концу
 			} else if (this.position < this.sliderEnd()) {
 				this.animateSlider(this.elemSlider, this.sliderEnd());
 				this.position = this.sliderEnd();
-				this.showSlideStep();
+				this.setSlideStep();
 				// Если перетаскивание было значительным, перейти на следующий слайд
 			} else if (this.touchEnd - this.touchStart < -20) {
 				this.position = this.distance * (Math.floor(this.position / this.distance));
-				if (this.flagSliderCount) {
-					this.showSlideStep();
-				}
+
+				this.setSlideStep();
 				this.animateSlider(this.elemSlider, this.position);
+
 				// Если перетаскивание было значительным, перейти на предыдущий слайд
 			} else if (this.touchEnd - this.touchStart > 20) {
 				this.position = this.distance * (Math.ceil(this.position / this.distance));
-				if (this.flagSliderCount) {
-					this.showSlideStep();
-				}
+
+				this.setSlideStep();
 				this.animateSlider(this.elemSlider, this.position);
 				// Иначе, оставить на текущем слайде
+
 			} else {
 				this.position = this.distance * (Math.round(this.position / this.distance));
 				this.animateSlider(this.elemSlider, this.position);
 			}
 			this.updateButtonStates();
-			this.dispatchSlideChangeEvent(); 
+			this.dispatchSlideChangeEvent();
 		}, 100);
 	}
 
@@ -290,44 +260,19 @@ class Slider {
 		return -(this.sliderLength * this.distance - this.visibleSlides * this.distance);
 	}
 
-	//Метод проверки кнопок для отключения или включения
-	updateButtonStates() {
-		if (this.elemBtnNext) {
-			this.elemBtnNext.disabled = this.position <= this.sliderEnd();
-		}
-		if (this.elemBtnPrev) {
-			this.elemBtnPrev.disabled = this.position >= 0;
-		}
-	}
 	// Метод установки общего количества шагов
-	setCountTotalStep() {
+	setTotalSteps() {
 		this.totalSteps = this.sliderLength - this.visibleSlides + 1;
-		return this.totalSteps
 	}
 
-	// Метод отображения общего количества шагов
-	showTotalStep() {
-		if (!this.flagSliderNumberStep) return;
-		this.elemTotalStep.textContent = this.totalSteps;
-	}
-
-	// Метод отображения текущего шага
-	showSlideStep() {
-
+	// Метод установки текущего шага
+	setSlideStep() {
 		if (this.position > 0) return;
-
 		const valueStep = Math.abs(this.position / this.distance) + 1;
 		this.stepNumber = valueStep;
 
-		if (!this.flagSliderNumberStep) return;
-		this.callBack(valueStep)
-
-		if (valueStep >= +this.elemTotalStep.textContent) {
-			this.elemStepSlide.textContent = this.elemTotalStep.textContent;
-			return;
-		}
-
-		this.elemStepSlide.textContent = valueStep;
+		this.callBackSteps(this.stepNumber, this.totalSteps);
+		this.dispatchSlideChangeEvent();
 	}
 
 	// Метод получения количества видимых слайдов по медиа-запросам
@@ -344,20 +289,30 @@ class Slider {
 		return this.elemItem.offsetWidth;
 	}
 
+	//Метод проверки кнопок для отключения или включения
+	updateButtonStates() {
+		if (this.elemBtnNext) {
+			this.elemBtnNext.disabled = this.position <= this.sliderEnd();
+		}
+		if (this.elemBtnPrev) {
+			this.elemBtnPrev.disabled = this.position >= 0;
+		}
+	}
+
 	// Метод перехода к следующему слайду
 	moveNext() {
 		const valueEnd = this.visibleSlides * this.distance - this.distance * this.sliderLength;
 		if (this.position <= valueEnd) return;
-	
+
 		this.position = this.position - this.distance;
 		this.animateSlider(this.elemSlider, this.position);
 	}
 
 	// Метод перехода к предыдущему слайду
 	movePrev() {
-		
+
 		if (this.position === 0) return;
-	
+
 		this.position = this.position + this.distance;
 		this.animateSlider(this.elemSlider, this.position);
 	}
@@ -371,18 +326,16 @@ class Slider {
 
 	// Метод сброса слайдера
 	resetSlider() {
+		this.stepNumber = 1;
 		this.position = 0;
-		if (this.flagSliderNumberStep) {
-			this.elemStepSlide.textContent = 1;
-		}
-
 		this.animateSlider(this.elemSlider, this.position);
 	}
 }
 
 
 
-// Объект медиа-запросов, в ключах прописываем сколько видно слайдов, в css устанавливаем какое количество слайдов видно .item {
+// Объект медиа-запросов, в ключах прописываем сколько видно слайдов, в css устанавливаем какое количество слайдов  видно,  например:
+//  .item {
 // 	flex-shrink: 0;
 // 	flex-grow: 0;
 // 	width: 25%; (25% это 4 слайда , 50% = 2 , 33% = 3, и тд)
@@ -395,47 +348,82 @@ const media = {
 	5: window.matchMedia('(min-width: 1025px)'),
 };
 
-// Элементы для отображения общего количества шагов и текущего шага
-const totalStep = document.querySelectorAll('.total-steps');
-const stepSlide = document.querySelectorAll('.step-slide');
-
-
-
-
+// Можно создать много слайдеров, для этого добавляем container-slider с его дочерними элементами в html
 document.querySelectorAll('.container-slider').forEach((elem, index) => {
 	// Объект с элементами слайдера, если кнопки ненужны не указываем их в объекте
 	const $sliderAllElem = {
 		btnNext: document.querySelectorAll('.btn-next-slide')[index],
-		btnPrev: document.querySelectorAll('.btn-prev-slide')[index], //КНОПКИ ЕСЛИ НЕ НУЖНЫ ТО ПРОСТО НЕ ПЕРЕДАЕМ
-		// containerSlider: elem,
+		btnPrev: document.querySelectorAll('.btn-prev-slide')[index],
 		slider: elem.querySelector('.slider'),
 		itemLength: elem.querySelectorAll('.item').length,
 		item: elem.querySelector('.item'),
 	}
+	const $containerIconsSteps = elem.querySelector('.container-icons-steps'); // контейнер с иконками шагов
+	const $totalSteps = elem.querySelector('.total-steps'); // элемент общего кол-ва шагов
+	const $stepSlide = elem.querySelector('.step-slide'); // элемент текущего шага
+
+
+
 	const sliderObj = new Slider(media);
 	sliderObj.initSlider($sliderAllElem);//инициализация слайдера
-
-
-	sliderObj.initCount(totalStep[index], stepSlide[index]);//ИНИЦИАЛИЗАЦИЯ СЧЕТЧИКА.НУЖНО ВЫЗВАТЬ ДЛЯ ОТОБРАЖЕНИЯ ШАГОВ. ЕСЛИ ПЕРЕДАТЬ АРГУМЕНТЫ  totalStep stepSlide ТО БУДУТ ОТОБРАЖАТЬСЯ ШАГИ 1 ИЗ N. ЕСЛИ НУЖНЫ ТОЛЬКО ИКОНКИ ШАГОВ ТО НЕ ПЕРЕДАЕМ АРГУМЕНТЫ А ВЫЗЫВАЕМ ПРОСТО МЕТОД sliderObj.initCount() И ПОТОМ ИНИЦИАЛИЗИРУЕМ ИКОНКИ ШАГОВ С ПОМОЩЬЮ МЕТОДА // sliderObj.initIconCount(allIconsCount, 'radio-active'); - allIconsCount ВСЕ ИКОНКИ ШАГОВ, radio-active КЛАСС КОТОРЫЙ БУДЕТ МЕНЯТЬ СОСТОЯНИЕ ИКОНКИ КОГДА ОНА АКТИВНА НА ТЕКУЩЕМ ШАГЕ---НУЖНО РЕАЛИЗОВАТЬ АВТОМАТИЧЕСКИЙ ПОДСЧЕТ КОЛИЧЕСТВА ИКОНОК ПОКА ЧТО КОЛИЧЕСТВО ИКОНОК ДОЛЖНО БЫТЬ РАВНО КОЛИЧЕСТВУ СЛАЙДОВ ПРОПИСАННЫХ ВРУЧНУЮ В HTML
-
-
-
-	// sliderObj.initIconCount(allIconsCount, 'radio-active');
-
 	sliderObj.initDragDrop('desktop');//инициализация drag'n drop не обязательна, если для desktop ненужно, то вызываем метод без аргумента
-sliderObj.initStepCallback(showStepIcon);
+	sliderObj.initStepsCallback(callbackSteps);
+	sliderObj.initResizeCallback(callbackResizeWidth);
+	
 
-// Подписка на событие
-sliderObj.elemSlider.addEventListener('slideChanged', (e) => {
-	console.log(`Событие: текущий шаг ${e.detail.currentStep}, всего шагов ${e.detail.totalSteps}`);
+	
+	if ($containerIconsSteps && sliderObj.totalSteps) {
+		$containerIconsSteps.append(initIconSteps(sliderObj.totalSteps, 'icon-step'))
+	}
+
+	// если нужно слайдер создает событие при изменение 
+	
+	elem.addEventListener('slideChanged', (e) => {
+		console.log(`Событие: текущий шаг ${e.detail.currentStep}, всего шагов ${e.detail.totalSteps}`);
+	});
+
+	// callback вызывается при изменении размеров экрана
+	function callbackResizeWidth() {
+		
+		if($containerIconsSteps && $containerIconsSteps.childNodes.length > 0) {
+			$containerIconsSteps.replaceChildren();
+			$containerIconsSteps.append(initIconSteps(sliderObj.totalSteps, 'icon-step'));
+			showStepIcon(1);
+		}
+		
+		
+		
+	}
+	// callback вызывается при изменении размеров экрана и при изменение позиции слайдера
+	function callbackSteps(step, totalSteps) {
+		if ($containerIconsSteps) {
+			showStepIcon(step);
+		}
+		showStepText(step, totalSteps);
+	}
+	// создании иконок шагов
+	function initIconSteps(length, className = null) {
+		const fragmentIcons = document.createDocumentFragment();
+
+		for (let index = 0; index < length; index++) {
+			const icon = document.createElement('div');
+			className && icon.classList.add(className);
+			fragmentIcons.appendChild(icon);
+		}
+		return fragmentIcons;
+	}
+	// переключение active  иконки шагов
+	function showStepIcon(step) {
+		const $allIconsSteps = $containerIconsSteps.querySelectorAll('.icon-step')
+	
+		$allIconsSteps.forEach(el => el.classList.remove('icon-active'));
+		$allIconsSteps[step - 1].classList.add('icon-active')
+	}
+	// показывает текущий шаг и всего количество шагов 
+	function showStepText(step, totalSteps) {
+		$stepSlide.textContent = step;
+		$totalSteps.textContent = totalSteps;
+	}
 });
 
 
-})
-
-// калбек для отслеживания шагов
-function showStepIcon(step) {
-	const iconsStep = document.querySelectorAll('.radio');
-	iconsStep.forEach(el => el.classList.remove('radio-active'));
-	iconsStep[step-1].classList.add('radio-active')
-}
